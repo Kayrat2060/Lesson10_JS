fd80c296ddfa00e90bfbf41a0afbab81d64dac21
Ayrat <aironur@yandex.ru>
2023-06-10 06:20:08 +0500

    001

---
+++ data.js
@@ -0,0 +1,30 @@
+const data = `
+{
+    "people" : [
+      {
+        "photo": "https://i.pinimg.com/236x/b6/28/1e/b6281ef3dabad07e68d55497cc74d08a.jpg",
+        "firstName": "Гарри",
+         "lastName": "Поттер",
+         "gender": "male",
+         "age": 28,
+         "number": "7349282382"
+      },
+      {
+        "photo": "https://i.pinimg.com/236x/69/b7/d9/69b7d94d8b8800a75b966824566f0c81--hermione-granger-costume-emma-watson.jpg", 
+        "firstName": "Гермиона",
+         "lastName": "Грейнджер",
+         "gender": "female",
+         "age": 24,
+         "number": "456754675"
+      },
+      {
+        "photo": "http://www.world-art.ru/cinema/img/20000/10329/vz8r.jpg", 
+        "firstName": "Рон",
+         "lastName": "Уизли",
+         "gender": "male",
+         "age": 32,
+         "number": "5678568567"
+      }
+    ]
+  }
+`
